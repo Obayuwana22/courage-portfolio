26eb97e70ab6690522460a384f069e31c6efb13b
Obayuwana22 <successobayuwana@gmail.com>
2025-04-09 11:25:56 +0100

    Add cursor to mode changer

--- src/components/DarkModeToggle.jsx
+++ src/components/DarkModeToggle.jsx
@@ -5,16 +5,18 @@ import { IoIosMoon } from "react-icons/io";
 const DarkModeToggle = () => {
   const { isDarkMode, toggleDarkMode } = useTheme();
   return (
-    <div className="">
-      <div className="flex flex-col items-center">
-        <button onClick={toggleDarkMode}>
-          {isDarkMode ? <LuSun color="white" size={20}/> : <IoIosMoon size={20} />}
-        </button>
-        <div className="italic text-[8px] lg:text-[10px] text-black dark:text-white">
-          mood swing
-        </div>
+    <button className="flex flex-col items-center cursor-pointer">
+      <div onClick={toggleDarkMode}>
+        {isDarkMode ? (
+          <LuSun color="white" size={20} />
+        ) : (
+          <IoIosMoon size={20} />
+        )}
       </div>
-    </div>
+      <div className="italic text-[8px] lg:text-[10px] text-black dark:text-white">
+        mood swing
+      </div>
+    </button>
   );
 };
 export default DarkModeToggle;
