c0927681f8be7a03a6ac6d8781c18c12e06c6ff0
Obayuwana22 <successobayuwana@gmail.com>
2025-04-10 01:23:18 +0100

    Add other projects new image format

--- src/data.js
+++ src/data.js
@@ -36,74 +36,74 @@ import dfw20 from "./assets/DYING FOR WATER/Stills/dfw20.webp";
 import dfw21 from "./assets/DYING FOR WATER/Stills/dfw21.webp";
 
 // // Project two assests
-// import ibrahim1 from "./assets/IBRAHIM/Stills/IBRAHIM1.webp";
-// import ibrahim2 from "./assets/IBRAHIM/Stills/IBRAHIM2.webp";
-// import ibrahim3 from "./assets/IBRAHIM/Stills/IBRAHIM3.webp";
-// import ibrahim4 from "./assets/IBRAHIM/Stills/IBRAHIM4.webp";
-// import ibrahim5 from "./assets/IBRAHIM/Stills/IBRAHIM5.webp";
-// import ibrahim6 from "./assets/IBRAHIM/Stills/IBRAHIM6.webp";
-// import ibrahim7 from "./assets/IBRAHIM/Stills/IBRAHIM7.webp";
-// import ibrahim8 from "./assets/IBRAHIM/Stills/IBRAHIM8.webp";
-// import ibrahim9 from "./assets/IBRAHIM/Stills/IBRAHIM9.webp";
-// import ibrahim10 from "./assets/IBRAHIM/Stills/IBRAHIM10.webp";
-// import ibrahim11 from "./assets/IBRAHIM/Stills/IBRAHIM11.webp";
-// import ibrahim12 from "./assets/IBRAHIM/Stills/IBRAHIM12.webp";
-// import ibrahim13 from "./assets/IBRAHIM/Stills/IBRAHIM13.webp";
-// import ibrahim14 from "./assets/IBRAHIM/Stills/IBRAHIM14.webp";
-// import ibrahim15 from "./assets/IBRAHIM/Stills/IBRAHIM15.webp";
-// import ibrahim16 from "./assets/IBRAHIM/Stills/IBRAHIM16.webp";
-// import ibrahim17 from "./assets/IBRAHIM/Stills/IBRAHIM17.webp";
-// import ibrahim18 from "./assets/IBRAHIM/Stills/IBRAHIM18.webp";
-// import ibrahim19 from "./assets/IBRAHIM/Stills/IBRAHIM19.webp";
+import ibrahim1 from "./assets/IBRAHIM/Stills/IBRAHIM1.webp";
+import ibrahim2 from "./assets/IBRAHIM/Stills/IBRAHIM2.webp";
+import ibrahim3 from "./assets/IBRAHIM/Stills/IBRAHIM3.webp";
+import ibrahim4 from "./assets/IBRAHIM/Stills/IBRAHIM4.webp";
+import ibrahim5 from "./assets/IBRAHIM/Stills/IBRAHIM5.webp";
+import ibrahim6 from "./assets/IBRAHIM/Stills/IBRAHIM6.webp";
+import ibrahim7 from "./assets/IBRAHIM/Stills/IBRAHIM7.webp";
+import ibrahim8 from "./assets/IBRAHIM/Stills/IBRAHIM8.webp";
+import ibrahim9 from "./assets/IBRAHIM/Stills/IBRAHIM9.webp";
+import ibrahim10 from "./assets/IBRAHIM/Stills/IBRAHIM10.webp";
+import ibrahim11 from "./assets/IBRAHIM/Stills/IBRAHIM11.webp";
+import ibrahim12 from "./assets/IBRAHIM/Stills/IBRAHIM12.webp";
+import ibrahim13 from "./assets/IBRAHIM/Stills/IBRAHIM13.webp";
+import ibrahim14 from "./assets/IBRAHIM/Stills/IBRAHIM14.webp";
+import ibrahim15 from "./assets/IBRAHIM/Stills/IBRAHIM15.webp";
+import ibrahim16 from "./assets/IBRAHIM/Stills/IBRAHIM16.webp";
+import ibrahim17 from "./assets/IBRAHIM/Stills/IBRAHIM17.webp";
+import ibrahim18 from "./assets/IBRAHIM/Stills/IBRAHIM18.webp";
+import ibrahim19 from "./assets/IBRAHIM/Stills/IBRAHIM19.webp";
 
 // // Project three assests
-// import idia1 from "./assets/IDIA/Stills/idia1.webp";
-// import idia2 from "./assets/IDIA/Stills/idia2.webp";
-// import idia3 from "./assets/IDIA/Stills/idia3.webp";
-// import idia4 from "./assets/IDIA/Stills/idia4.webp";
-// import idia5 from "./assets/IDIA/Stills/idia5.webp";
-// import idia6 from "./assets/IDIA/Stills/idia6.webp";
-// import idia7 from "./assets/IDIA/Stills/idia7.webp";
-// import idia8 from "./assets/IDIA/Stills/idia8.webp";
-// import idia9 from "./assets/IDIA/Stills/idia9.webp";
-// import idia10 from "./assets/IDIA/Stills/idia10.webp";
-// import idia11 from "./assets/IDIA/Stills/idia11.webp";
-// import idia12 from "./assets/IDIA/Stills/idia12.webp";
-// import idia13 from "./assets/IDIA/Stills/idia13.webp";
-// import idia14 from "./assets/IDIA/Stills/idia14.webp";
-// import idia15 from "./assets/IDIA/Stills/idia15.webp";
-// import idia16 from "./assets/IDIA/Stills/idia16.webp";
+import idia1 from "./assets/IDIA/Stills/idia1.webp";
+import idia2 from "./assets/IDIA/Stills/idia2.webp";
+import idia3 from "./assets/IDIA/Stills/idia3.webp";
+import idia4 from "./assets/IDIA/Stills/idia4.webp";
+import idia5 from "./assets/IDIA/Stills/idia5.webp";
+import idia6 from "./assets/IDIA/Stills/idia6.webp";
+import idia7 from "./assets/IDIA/Stills/idia7.webp";
+import idia8 from "./assets/IDIA/Stills/idia8.webp";
+import idia9 from "./assets/IDIA/Stills/idia9.webp";
+import idia10 from "./assets/IDIA/Stills/idia10.webp";
+import idia11 from "./assets/IDIA/Stills/idia11.webp";
+import idia12 from "./assets/IDIA/Stills/idia12.webp";
+import idia13 from "./assets/IDIA/Stills/idia13.webp";
+import idia14 from "./assets/IDIA/Stills/idia14.webp";
+import idia15 from "./assets/IDIA/Stills/idia15.webp";
+import idia16 from "./assets/IDIA/Stills/idia16.webp";
 
 // // Project four assests
-// import maji1 from "./assets/PROJECT MAJI/Stills/maji1.webp";
-// import maji2 from "./assets/PROJECT MAJI/Stills/maji2.webp";
-// import maji3 from "./assets/PROJECT MAJI/Stills/maji3.webp";
-// import maji4 from "./assets/PROJECT MAJI/Stills/maji4.webp";
-// import maji5 from "./assets/PROJECT MAJI/Stills/maji5.webp";
-// import maji6 from "./assets/PROJECT MAJI/Stills/maji6.webp";
-// import maji7 from "./assets/PROJECT MAJI/Stills/maji7.webp";
-// import maji8 from "./assets/PROJECT MAJI/Stills/maji8.webp";
-// import maji9 from "./assets/PROJECT MAJI/Stills/maji9.webp";
-// import maji10 from "./assets/PROJECT MAJI/Stills/maji10.webp";
-// import maji11 from "./assets/PROJECT MAJI/Stills/maji11.webp";
-// import maji12 from "./assets/PROJECT MAJI/Stills/maji12.webp";
-// import maji13 from "./assets/PROJECT MAJI/Stills/maji13.webp";
-// import maji14 from "./assets/PROJECT MAJI/Stills/maji14.webp";
-// import maji15 from "./assets/PROJECT MAJI/Stills/maji15.webp";
-// import maji16 from "./assets/PROJECT MAJI/Stills/maji16.webp";
-// import maji17 from "./assets/PROJECT MAJI/Stills/maji17.webp";
-// import maji18 from "./assets/PROJECT MAJI/Stills/maji18.webp";
-// import maji19 from "./assets/PROJECT MAJI/Stills/maji19.webp";
-// import maji20 from "./assets/PROJECT MAJI/Stills/maji20.webp";
-// import maji21 from "./assets/PROJECT MAJI/Stills/maji21.webp";
-// import maji22 from "./assets/PROJECT MAJI/Stills/maji22.webp";
-// import maji23 from "./assets/PROJECT MAJI/Stills/maji23.webp";
-// import maji24 from "./assets/PROJECT MAJI/Stills/maji24.webp";
-// import maji25 from "./assets/PROJECT MAJI/Stills/maji25.webp";
-// import maji26 from "./assets/PROJECT MAJI/Stills/maji26.webp";
-// import maji27 from "./assets/PROJECT MAJI/Stills/maji27.webp";
-// import maji28 from "./assets/PROJECT MAJI/Stills/maji28.webp";
-// import maji29 from "./assets/PROJECT MAJI/Stills/maji29.webp";
+import maji1 from "./assets/PROJECT MAJI/Stills/maji1.webp";
+import maji2 from "./assets/PROJECT MAJI/Stills/maji2.webp";
+import maji3 from "./assets/PROJECT MAJI/Stills/maji3.webp";
+import maji4 from "./assets/PROJECT MAJI/Stills/maji4.webp";
+import maji5 from "./assets/PROJECT MAJI/Stills/maji5.webp";
+import maji6 from "./assets/PROJECT MAJI/Stills/maji6.webp";
+import maji7 from "./assets/PROJECT MAJI/Stills/maji7.webp";
+import maji8 from "./assets/PROJECT MAJI/Stills/maji8.webp";
+import maji9 from "./assets/PROJECT MAJI/Stills/maji9.webp";
+import maji10 from "./assets/PROJECT MAJI/Stills/maji10.webp";
+import maji11 from "./assets/PROJECT MAJI/Stills/maji11.webp";
+import maji12 from "./assets/PROJECT MAJI/Stills/maji12.webp";
+import maji13 from "./assets/PROJECT MAJI/Stills/maji13.webp";
+import maji14 from "./assets/PROJECT MAJI/Stills/maji14.webp";
+import maji15 from "./assets/PROJECT MAJI/Stills/maji15.webp";
+import maji16 from "./assets/PROJECT MAJI/Stills/maji16.webp";
+import maji17 from "./assets/PROJECT MAJI/Stills/maji17.webp";
+import maji18 from "./assets/PROJECT MAJI/Stills/maji18.webp";
+import maji19 from "./assets/PROJECT MAJI/Stills/maji19.webp";
+import maji20 from "./assets/PROJECT MAJI/Stills/maji20.webp";
+import maji21 from "./assets/PROJECT MAJI/Stills/maji21.webp";
+import maji22 from "./assets/PROJECT MAJI/Stills/maji22.webp";
+import maji23 from "./assets/PROJECT MAJI/Stills/maji23.webp";
+import maji24 from "./assets/PROJECT MAJI/Stills/maji24.webp";
+import maji25 from "./assets/PROJECT MAJI/Stills/maji25.webp";
+import maji26 from "./assets/PROJECT MAJI/Stills/maji26.webp";
+import maji27 from "./assets/PROJECT MAJI/Stills/maji27.webp";
+import maji28 from "./assets/PROJECT MAJI/Stills/maji28.webp";
+import maji29 from "./assets/PROJECT MAJI/Stills/maji29.webp";
 
 export const projects = [
   {
@@ -147,129 +147,129 @@ export const projects = [
       img21: dfw21,
     },
   },
-  // {
-  //   id: nanoid(),
-  //   img: ibrahim1,
-  //   title: "ibrahim",
-  //   service: "short film",
-  //   youTubeLink: "https://www.youtube.com/embed/syHSnmPaBaE",
-  //   projectInformation: {
-  //     title: "Ibrahim",
-  //     clientName: "Just Red Production",
-  //     directorDPName: "Yemi Omu",
-  //     beforeAfterLabel: "Before | After",
-  //   },
-  //   callToAction: {
-  //     contactDetails:
-  //       "Available for freelance work. Contact: courage304@gmail.com",
-  //     socialMediaLinks: "Follow @yourhandle on Instagram",
-  //   },
-  //   gallery: {
-  //     img1: ibrahim1,
-  //     img2: ibrahim2,
-  //     img3: ibrahim3,
-  //     img4: ibrahim4,
-  //     img5: ibrahim5,
-  //     img6: ibrahim6,
-  //     img7: ibrahim7,
-  //     img8: ibrahim8,
-  //     img9: ibrahim9,
-  //     img10: ibrahim10,
-  //     img11: ibrahim11,
-  //     img12: ibrahim12,
-  //     img13: ibrahim13,
-  //     img14: ibrahim14,
-  //     img15: ibrahim15,
-  //     img16: ibrahim16,
-  //     img17: ibrahim17,
-  //     img18: ibrahim18,
-  //     img19: ibrahim19,
-  //   },
-  // },
-  // {
-  //   id: nanoid(),
-  //   img: idia8,
-  //   title: "idia",
-  //   service: "feature film",
-  //   video: dfwVideo,
-  //   projectInformation: {
-  //     title: "Idia",
-  //     clientName: "Just Red Production",
-  //     directorDPName: "Yemi Omu",
-  //     beforeAfterLabel: "Before | After",
-  //   },
-  //   callToAction: {
-  //     contactDetails:
-  //       "Available for freelance work. Contact: courage304@gmail.com",
-  //     socialMediaLinks: "Follow @yourhandle on Instagram",
-  //   },
-  //   gallery: {
-  //     img1: idia1,
-  //     img2: idia2,
-  //     img3: idia3,
-  //     img4: idia4,
-  //     img5: idia5,
-  //     img6: idia6,
-  //     img7: idia7,
-  //     img8: idia8,
-  //     img9: idia9,
-  //     img10: idia10,
-  //     img11: idia11,
-  //     img12: idia12,
-  //     img13: idia13,
-  //     img14: idia14,
-  //     img15: idia15,
-  //     img16: idia16,
-  //   },
-  // },
+  {
+    id: nanoid(),
+    img: ibrahim1,
+    title: "ibrahim",
+    service: "short film",
+    youTubeLink: "https://www.youtube.com/embed/syHSnmPaBaE",
+    projectInformation: {
+      title: "Ibrahim",
+      clientName: "Just Red Production",
+      directorDPName: "Yemi Omu",
+      beforeAfterLabel: "Before | After",
+    },
+    callToAction: {
+      contactDetails:
+        "Available for freelance work. Contact: courage304@gmail.com",
+      socialMediaLinks: "Follow @yourhandle on Instagram",
+    },
+    gallery: {
+      img1: ibrahim1,
+      img2: ibrahim2,
+      img3: ibrahim3,
+      img4: ibrahim4,
+      img5: ibrahim5,
+      img6: ibrahim6,
+      img7: ibrahim7,
+      img8: ibrahim8,
+      img9: ibrahim9,
+      img10: ibrahim10,
+      img11: ibrahim11,
+      img12: ibrahim12,
+      img13: ibrahim13,
+      img14: ibrahim14,
+      img15: ibrahim15,
+      img16: ibrahim16,
+      img17: ibrahim17,
+      img18: ibrahim18,
+      img19: ibrahim19,
+    },
+  },
+  {
+    id: nanoid(),
+    img: idia8,
+    title: "idia",
+    service: "feature film",
+    video: dfwVideo,
+    projectInformation: {
+      title: "Idia",
+      clientName: "Just Red Production",
+      directorDPName: "Yemi Omu",
+      beforeAfterLabel: "Before | After",
+    },
+    callToAction: {
+      contactDetails:
+        "Available for freelance work. Contact: courage304@gmail.com",
+      socialMediaLinks: "Follow @yourhandle on Instagram",
+    },
+    gallery: {
+      img1: idia1,
+      img2: idia2,
+      img3: idia3,
+      img4: idia4,
+      img5: idia5,
+      img6: idia6,
+      img7: idia7,
+      img8: idia8,
+      img9: idia9,
+      img10: idia10,
+      img11: idia11,
+      img12: idia12,
+      img13: idia13,
+      img14: idia14,
+      img15: idia15,
+      img16: idia16,
+    },
+  },
 
-  // {
-  //   id: nanoid(),
-  //   img: maji9,
-  //   title: "project maji",
-  //   service: "documentary",
-  //   video: dfwVideo,
-  //   projectInformation: {
-  //     title: "Project Maji",
-  //     clientName: "Just Red Production",
-  //     directorDPName: "Yemi Omu",
-  //     beforeAfterLabel: "Before | After",
-  //   },
-  //   callToAction: {
-  //     contactDetails:
-  //       "Available for freelance work. Contact: courage304@gmail.com",
-  //     socialMediaLinks: "Follow @yourhandle on Instagram",
-  //   },
-  //   gallery: {
-  //     img1: maji1,
-  //     img2: maji2,
-  //     img3: maji3,
-  //     img4: maji4,
-  //     img5: maji5,
-  //     img6: maji6,
-  //     img7: maji7,
-  //     img8: maji8,
-  //     img9: maji9,
-  //     img10: maji10,
-  //     img11: maji11,
-  //     img12: maji12,
-  //     img13: maji13,
-  //     img14: maji14,
-  //     img15: maji15,
-  //     img16: maji16,
-  //     img17: maji17,
-  //     img18: maji18,
-  //     img19: maji19,
-  //     img20: maji20,
-  //     img21: maji21,
-  //     img22: maji22,
-  //     img23: maji23,
-  //     img24: maji24,
-  //     img25: maji25,
-  //     img26: maji26,
-  //     img27: maji27,
-  //     img28: maji28,
-  //     img29: maji29,
-  //   },
-  // },
+  {
+    id: nanoid(),
+    img: maji9,
+    title: "project maji",
+    service: "documentary",
+    video: dfwVideo,
+    projectInformation: {
+      title: "Project Maji",
+      clientName: "Just Red Production",
+      directorDPName: "Yemi Omu",
+      beforeAfterLabel: "Before | After",
+    },
+    callToAction: {
+      contactDetails:
+        "Available for freelance work. Contact: courage304@gmail.com",
+      socialMediaLinks: "Follow @yourhandle on Instagram",
+    },
+    gallery: {
+      img1: maji1,
+      img2: maji2,
+      img3: maji3,
+      img4: maji4,
+      img5: maji5,
+      img6: maji6,
+      img7: maji7,
+      img8: maji8,
+      img9: maji9,
+      img10: maji10,
+      img11: maji11,
+      img12: maji12,
+      img13: maji13,
+      img14: maji14,
+      img15: maji15,
+      img16: maji16,
+      img17: maji17,
+      img18: maji18,
+      img19: maji19,
+      img20: maji20,
+      img21: maji21,
+      img22: maji22,
+      img23: maji23,
+      img24: maji24,
+      img25: maji25,
+      img26: maji26,
+      img27: maji27,
+      img28: maji28,
+      img29: maji29,
+    },
+  },
 ];
