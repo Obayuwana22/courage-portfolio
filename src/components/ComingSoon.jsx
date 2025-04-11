7c56944b5e76e61a603c5b60ac32522db4ef96de
Obayuwana22 <successobayuwana@gmail.com>
2025-04-11 09:51:43 +0100

    Modify coming soon text

--- src/components/ComingSoon.jsx
+++ src/components/ComingSoon.jsx
@@ -1,7 +1,7 @@
 const ComingSoon = () => {
   return (
     <div className="text-center mt-20">
-      <div className="italic font-medium">Still brewing...Anticipate.</div>
+      <div className="italic font-medium text-black dark:text-white">Still brewing...Anticipate.</div>
     </div>
   );
 };
