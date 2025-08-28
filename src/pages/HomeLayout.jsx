e7aab993a1dd1a470d3dcdc97dd78eb4f479a9a0
Success Obayuwana <succcessful.obayuwana@charisol.io>
2025-08-28 23:57:26 +0100

    testing

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -7,9 +7,9 @@ const HomeLayout = () => {
   return (
     <div className="flex flex-col mx-5 min-h-screen">
       <header className="flex flex-col items-center text-center mt-10 ">
-        {/* <div className="flex items-center justify-center border-4 rounded-full w-[70px] h-[70px] text-2xl font-bold">
+        <div className="flex items-center justify-center border-4 rounded-full w-[70px] h-[70px] text-2xl font-bold text-red-500">
           CEE
-        </div> */}
+        </div>
         <Link to="/">
           <h1 className="text-2xl tracking-[0.4em] font-medium mt-2 text-black dark:text-white">
             COURAGE OBAYUWANA
